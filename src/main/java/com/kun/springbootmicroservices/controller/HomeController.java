12e002a150833d00e7ea4f1ad6a1b49682f1b281
Kun Xia <kn991666@dal.ca>
2023-05-18 21:25:16 +0800

    path variable added

--- src/main/java/com/kun/springbootmicroservices/controller/HomeController.java
+++ src/main/java/com/kun/springbootmicroservices/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.kun.springbootmicroservices.controller;
 
+import com.kun.springbootmicroservices.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
         return "Hello World!";
+    }
+
+    // @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId(1);
+        user.setName("Kun");
+        user.setEmail("kun@gmail.com");
+
+        return user;
+    }
+    @GetMapping("/{id1}/{id2}")
+    public String pathVariable(@PathVariable int id1, @PathVariable("id2") String name){
+        return "The id is: " + id1 + ", the name is: " + name;
     }
 }
