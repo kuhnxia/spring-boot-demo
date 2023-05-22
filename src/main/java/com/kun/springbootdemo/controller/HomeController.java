7cedfc912f8a168679ea50aafd37d7e4a46d968c
Kun Xia <kn991666@dal.ca>
2023-05-22 13:49:48 +0800

    Reset

---
+++ src/main/java/com/kun/springbootdemo/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.kun.springbootdemo.controller;
+
+import com.kun.springbootdemo.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    // @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
+    public User user(){
+        User user = new User();
+        user.setId("1");
+        user.setName("Kun");
+        user.setEmail("kun@gmail.com");
+
+        return user;
+    }
+    @GetMapping("/{id1}/{id2}")
+    public String pathVariable(@PathVariable String id1, @PathVariable("id2") String name){
+        return "The id is: " + id1 + ", the name is: " + name;
+    }
+
+    @GetMapping("/requestParam")
+    public String requestParams(@RequestParam String name,
+                                @RequestParam(name = "email", required = false,defaultValue = "")
+                                String emailId){
+        return name + "'s email is: " + emailId;
+    }
+}
---
+++ src/main/java/com/kun/springbootdemo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.kun.springbootdemo.repository;
+
+import com.kun.springbootdemo.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, String> {
+}
